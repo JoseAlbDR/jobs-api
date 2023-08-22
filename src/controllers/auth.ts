b68cbf05dac2882a4ee4ad52d7c1a600b7033d05
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-22 16:26:13 +0200

    feat: hash password before save user in DB

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,9 +2,17 @@ import { Response, Request } from "express";
 import { IRegisterRequest } from "../types/interfaces";
 import { StatusCodes } from "http-status-codes";
 import { User } from "../Models/User";
-// import { User } from "../Models/User";
+import bcrypt from "bcryptjs";
+
 const register = async (req: IRegisterRequest, res: Response) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { name, email, password: hashedPassword };
+
+  const user = await User.create(tempUser);
 
   res.status(StatusCodes.CREATED).json({ user });
 };
