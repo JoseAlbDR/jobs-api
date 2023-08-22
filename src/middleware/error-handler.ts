1f4e25e447729d43153e2d5677d045c60bea5ebc
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-22 13:29:21 +0200

    feat: catch duplicate key MongoError to send back a custom message

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import CustomAPIError from "../errors/custom-error";
 import { StatusCodes } from "http-status-codes";
+import { MongoError } from "mongodb";
+
 const errorHandlerMiddleware = (
   err: Error,
   _req: Request,
@@ -10,6 +12,12 @@ const errorHandlerMiddleware = (
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
+  if ((err as MongoError).code === 11000) {
+    return res
+      .status(StatusCodes.CONFLICT)
+      .json({ msg: "Email Already exist in database", err });
+  }
+
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
 };
 
