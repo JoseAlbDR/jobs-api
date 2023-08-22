0bd714644245d916047192574718b5a72a2c6306
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-22 19:37:40 +0200

    feat: add IUserMethods interface with createJWT method

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,10 +1,16 @@
 import { Request } from "express";
+import { Document } from "mongoose";
 
 export interface IRegisterRequest extends Request {
   body: IUser;
 }
 
-export interface IUser {
+export interface IUserMethods {
+  createJWT(): string;
+}
+
+export interface IUser extends Document, IUserMethods {
+  _id: string;
   name: string;
   email: string;
   password: string;
