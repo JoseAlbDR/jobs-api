7cf805ebe577e9e452adc97850ec388a34aaa37a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-22 16:36:36 +0200

    refactor: move hashing password logic to mongoose pre middleware

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,17 +2,9 @@ import { Response, Request } from "express";
 import { IRegisterRequest } from "../types/interfaces";
 import { StatusCodes } from "http-status-codes";
 import { User } from "../Models/User";
-import bcrypt from "bcryptjs";
 
 const register = async (req: IRegisterRequest, res: Response) => {
-  const { name, email, password } = req.body;
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const tempUser = { name, email, password: hashedPassword };
-
-  const user = await User.create(tempUser);
+  const user = await User.create(req.body);
 
   res.status(StatusCodes.CREATED).json({ user });
 };
