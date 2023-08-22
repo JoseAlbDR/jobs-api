52ce3d320e69a9c2499bed348336092c115333c5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-23 00:50:34 +0200

    feat: use joi-validation middleware in login route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import { login, register } from "../controllers/auth";
-import validateRegistration from "../middleware/joi-validation";
+import validateBody from "../middleware/joi-validation";
 const router = express.Router();
 
-router.post("/login", login);
-router.post("/register", validateRegistration, register);
+router.post("/login", validateBody, login);
+router.post("/register", validateBody, register);
 
 export default router;
