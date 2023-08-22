a1f9b4c143f5df0a40d07284658e79b714e4e5b9
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-22 11:54:48 +0200

    feat: use validateRigistration middleware in register route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import { login, register } from "../controllers/auth";
-
+import validateRegistration from "../middleware/joi-validation";
 const router = express.Router();
 
 router.post("/login", login);
-router.post("/register", register);
+router.post("/register", validateRegistration, register);
 
 export default router;
