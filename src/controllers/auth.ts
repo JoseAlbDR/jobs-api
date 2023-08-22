b3ada3623eae16c4887a36ea90a9a5c4cb333ba9
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-22 13:29:46 +0200

    feat: create user with validate/required name, email, password

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,10 +1,12 @@
 import { Response, Request } from "express";
 import { IRegisterRequest } from "../types/interfaces";
+import { StatusCodes } from "http-status-codes";
+import { User } from "../Models/User";
 // import { User } from "../Models/User";
 const register = async (req: IRegisterRequest, res: Response) => {
-  const { name, email, password } = req.body;
-  console.log(name, email, password);
-  res.send("register user");
+  const user = await User.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (_req: Request, res: Response) => {
