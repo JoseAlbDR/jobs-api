86950f854949a81ad9e239f60e10ff04407d9d0a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-24 00:38:15 +0200

    feat: handle duplicate value (code: 11000) error in MongoDB

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import CustomAPIError from "../errors/custom-error";
 import { StatusCodes } from "http-status-codes";
 import { MongoError } from "mongodb";
+import { IMongoError } from "../types/interfaces";
 
 const errorHandlerMiddleware = (
   err: Error,
@@ -9,16 +10,32 @@ const errorHandlerMiddleware = (
   res: Response,
   _next: NextFunction
 ) => {
+  // const customError: ICustomError = {
+  //   // set default
+  //   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  //   statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+  //   msg: err.message || "Something went wrong try again later",
+  // };
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
-  if ((err as MongoError).code === 11000) {
-    return res
-      .status(StatusCodes.CONFLICT)
-      .json({ msg: "Email Already exist in database", err });
-  }
 
+  // if ((err as MongoError).code === 11000) {
+  //   return res
+  //     .status(StatusCodes.CONFLICT)
+  //     .json({ msg: `Duplicate value entered for ${err.keyValue}`, err });
+  // }
+
+  if (err instanceof MongoError && err.code === 11000) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      msg: `Duplicate value entered for ${Object.keys(
+        (err as IMongoError).keyValue
+      ).join(", ")} please enter a valid value`,
+    });
+  }
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  // return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 export default errorHandlerMiddleware;
