7fe8e0cea1ec616f684e26da217206795e6fb6f8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-23 02:05:53 +0200

    feat: check if user exist and passwords match in login function

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,8 @@
-import { Response, Request } from "express";
-import { IRegisterRequest } from "../types/interfaces";
+import { Response } from "express";
+import { ILoginRequest, IRegisterRequest } from "../types/interfaces";
 import { StatusCodes } from "http-status-codes";
 import { User } from "../Models/User";
+import { UnauthenticatedError } from "../errors";
 
 const register = async (req: IRegisterRequest, res: Response) => {
   const user = await User.create(req.body);
@@ -9,8 +10,22 @@ const register = async (req: IRegisterRequest, res: Response) => {
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
-const login = async (_req: Request, res: Response) => {
-  res.send("register login");
+const login = async (req: ILoginRequest, res: Response) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid email");
+  }
+
+  const isPasswordCorrect = await user.checkPassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Incorrect password");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 export { register, login };
