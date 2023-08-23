c828cbeaa6f20c9bd7cf962a208695f207603fc8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-23 10:20:31 +0200

    feat: new mongoose Model Job

---
+++ src/Models/Job.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { IJob } from "../types/interfaces";
+
+const JobSchema = new mongoose.Schema<IJob>(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Job = mongoose.model<IJob>("Job", JobSchema);
