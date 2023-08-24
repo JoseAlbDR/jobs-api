0ed331c40c641d4048395468033554818f7c26c4
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-24 02:09:46 +0200

    refactor: loginEmail to email, new string.pattern.base error message

--- src/utils/authValidation.ts
+++ src/utils/authValidation.ts
@@ -1,34 +1,34 @@
 import Joi from "joi";
 import { ILogin, IUser } from "../types/interfaces";
 
-export const validateRegisterData = (registerData: unknown) => {
+export const validateRegisterData = (data: unknown) => {
   const registerSchema = Joi.object<IUser>({
     name: Joi.string().required(),
     email: Joi.string().required(),
     password: Joi.string().required(),
   });
 
-  return registerSchema.validate(registerData, {
-    errors: { wrap: { label: false } },
+  return registerSchema.validate(data, {
+    errors: { label: false }, // Removed unnecessary 'wrap' property
     messages: {
       "any.required": "Please provide {#key}",
     },
   });
 };
 
-export const validateLoginData = (loginData: unknown) => {
+export const validateLoginData = (data: unknown) => {
   const pattern =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const loginSchema = Joi.object<ILogin>({
-    loginEmail: Joi.string().pattern(new RegExp(pattern)).required(),
+    email: Joi.string().pattern(new RegExp(pattern)).required(),
     password: Joi.string().min(6).required(),
   });
 
-  return loginSchema.validate(loginData, {
-    errors: { wrap: { label: false } },
+  return loginSchema.validate(data, {
+    errors: { label: false }, // Removed unnecessary 'wrap' property
     messages: {
       "any.required": "Please provide {#key}.",
-      "string.pattern.base": "Provide a valid email.",
+      "string.pattern.base": "Please provide a valid email.",
     },
   });
 };
