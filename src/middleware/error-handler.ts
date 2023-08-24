378e5ffedf01425c0709fdbdc98641111f857fb1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-24 11:21:07 +0200

    feat: add ValidationError to Mongoose handled errors

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -2,7 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import CustomAPIError from "../errors/custom-error";
 import { StatusCodes } from "http-status-codes";
 import { MongoError } from "mongodb";
-import { IMongoError } from "../types/interfaces";
+import { IDuplicateMongoError, IRequiredMongoError } from "../types/interfaces";
+import { Error } from "mongoose";
 
 const errorHandlerMiddleware = (
   err: Error,
@@ -14,10 +15,18 @@ const errorHandlerMiddleware = (
     return res.status(err.statusCode).json({ msg: err.message });
   }
 
+  if (err instanceof Error && err.name === "ValidationError") {
+    const { errors } = err as IRequiredMongoError;
+    const messages = Object.values(errors)
+      .map((item) => item.message)
+      .join(", ");
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg: messages });
+  }
+
   if (err instanceof MongoError && err.code === 11000) {
     return res.status(StatusCodes.BAD_REQUEST).json({
       msg: `Duplicate value entered for ${Object.keys(
-        (err as IMongoError).keyValue
+        (err as IDuplicateMongoError).keyValue
       ).join(", ")} please enter a valid value`,
     });
   }
