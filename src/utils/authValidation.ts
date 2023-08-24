7aa59bf52c3e454ca38d88543b767d750b8dab5e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-24 10:46:22 +0200

    feat: removed .required() to handle required fields error with mongodb

--- src/utils/authValidation.ts
+++ src/utils/authValidation.ts
@@ -3,16 +3,16 @@ import { CustomBody, ILogin, IUser } from "../types/interfaces";
 
 export const validateRegisterData = <T extends CustomBody>(data: T) => {
   const registerSchema = Joi.object<IUser>({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    password: Joi.string().required(),
+    name: Joi.string(),
+    email: Joi.string(),
+    password: Joi.string(),
   });
 
   return registerSchema.validate(data, {
     errors: { wrap: { label: false } },
-    messages: {
-      "any.required": "Please provide {#key}",
-    },
+    // messages: {
+    //   "any.required": "Please provide {#key}",
+    // },
   });
 };
 
@@ -20,15 +20,15 @@ export const validateLoginData = <T extends CustomBody>(data: T) => {
   const pattern =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const loginSchema = Joi.object<ILogin>({
-    email: Joi.string().pattern(new RegExp(pattern)).required(),
-    password: Joi.string().min(6).required(),
+    email: Joi.string().pattern(new RegExp(pattern)),
+    password: Joi.string().min(6),
   });
 
   return loginSchema.validate(data, {
     errors: { wrap: { label: false } },
     messages: {
       "object.unknown": "Unknown parameter: {#key}",
-      "any.required": "Please provide {#key}.",
+      // "any.required": "Please provide {#key}.",
       "string.pattern.base": "Please provide a valid email.",
     },
   });
--- src/utils/jobsValidation.ts
+++ src/utils/jobsValidation.ts
@@ -1,24 +1,24 @@
 import Joi from "joi";
-import { CustomBody, IJob } from "../types/interfaces";
+import { CustomBody, IJob, IUpdateJob } from "../types/interfaces";
 
 export const validateCreateJobData = <T extends CustomBody>(data: T) => {
   const jobSchema = Joi.object<IJob>({
-    company: Joi.string().required(),
-    position: Joi.string().required(),
+    company: Joi.string(),
+    position: Joi.string(),
     status: Joi.string(),
     createdBy: Joi.string().hex().length(24),
   });
 
   return jobSchema.validate(data, {
     errors: { wrap: { label: false } },
-    messages: {
-      "any.required": "Please provide {#key}",
-    },
+    // messages: {
+    //   "any.required": "Please provide {#key}",
+    // },
   });
 };
 
 export const validateUpdateJobData = <T extends CustomBody>(data: T) => {
-  const jobSchema = Joi.object<IJob>({
+  const jobSchema = Joi.object<IUpdateJob>({
     company: Joi.string(),
     position: Joi.string(),
     status: Joi.string(),
