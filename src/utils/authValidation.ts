31aa72ef441ad1ffbed0a45d30df2e41f70db63c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-24 11:17:49 +0200

    feat: required validation for LoginData and password set to min(6) characters in joi-validation

--- src/utils/authValidation.ts
+++ src/utils/authValidation.ts
@@ -5,31 +5,27 @@ export const validateRegisterData = <T extends CustomBody>(data: T) => {
   const registerSchema = Joi.object<IUser>({
     name: Joi.string(),
     email: Joi.string(),
-    password: Joi.string(),
+    password: Joi.string().min(6),
   });
 
   return registerSchema.validate(data, {
     errors: { wrap: { label: false } },
-    // messages: {
-    //   "any.required": "Please provide {#key}",
-    // },
+    messages: {
+      "object.unknown": "Unknown parameter: {#key}",
+    },
   });
 };
 
 export const validateLoginData = <T extends CustomBody>(data: T) => {
-  const pattern =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const loginSchema = Joi.object<ILogin>({
-    email: Joi.string().pattern(new RegExp(pattern)),
-    password: Joi.string().min(6),
+    email: Joi.string().required(),
+    password: Joi.string().required(),
   });
 
   return loginSchema.validate(data, {
     errors: { wrap: { label: false } },
     messages: {
       "object.unknown": "Unknown parameter: {#key}",
-      // "any.required": "Please provide {#key}.",
-      "string.pattern.base": "Please provide a valid email.",
     },
   });
 };
