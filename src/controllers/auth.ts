85a90c48f7585d59041f6fd476e988683dfd8389
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-25 02:53:47 +0200

    feat: send newly created lastName and location user properties along with token in auth and login routes

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -7,7 +7,15 @@ import { UnauthenticatedError } from "../errors";
 const register = async (req: IRegisterRequest, res: Response) => {
   const user = await User.create(req.body);
   const token = user.createJWT();
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      name: user.name,
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      token,
+    },
+  });
 };
 
 const login = async (req: ILoginRequest, res: Response) => {
@@ -25,7 +33,7 @@ const login = async (req: ILoginRequest, res: Response) => {
   }
 
   const token = user.createJWT();
-  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+  res.status(StatusCodes.OK).json({ user: { name: user.name, token } });
 };
 
 export { register, login };
