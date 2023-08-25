07e342287bd32b4f2e6ef5a0aa8e4f3bd82137ac
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-26 00:34:47 +0200

    feat: add validateQuery function to joi-validation middleware, export validateBody and validateQuery

--- src/middleware/joi-validation.ts
+++ src/middleware/joi-validation.ts
@@ -1,4 +1,5 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { validateJobQuery } from "../utils/queryValidation";
 import { CustomBody, CustomRequest } from "../types/interfaces";
 import { BadRequestError } from "../errors";
 import Joi from "joi";
@@ -23,4 +24,18 @@ const validateBody =
     next();
   };
 
-export default validateBody;
+const validateQuery = (req: Request, _res: Response, next: NextFunction) => {
+  const { query } = req;
+  const valid = validateJobQuery(query);
+
+  if (valid.error) {
+    const messages = valid.error.details.map(
+      (detail): string => detail.message
+    );
+    throw new BadRequestError(messages.join(" "));
+  }
+  req.jobQuery = valid.value;
+  return next();
+};
+
+export { validateBody, validateQuery };
